3dc870e5830decbc2a2b435f8b44e3e8814784ed
Barbara Mayfield <bmayfield1991@gmail.com>
2020-10-06 18:33:02 -0500

    work: expensive_seq.py finished

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,23 @@
 # Your code here
+# Hint: In Python, a dict key can be any immutable type...
+#       including a tuple.
+
+cache = {}
 
 
 def expensive_seq(x, y, z):
     # Your code here
+    # exps will be our keys in the dict
+    exps = (x, y, z)
 
+    # x, y, and z are all greater than 0
+    if x <= 0:
+        return y + z
+
+    if exps not in cache:
+        cache[(x, y, z)] = expensive_seq(x-1, y+1, z) + \
+            expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+    return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
