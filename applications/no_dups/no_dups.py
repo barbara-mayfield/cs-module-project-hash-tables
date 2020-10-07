954a627556eee9334cfb776604380bee80177ad3
Barbara Mayfield <bmayfield1991@gmail.com>
2020-10-06 19:16:31 -0500

    work: no_dups.py finished

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,20 @@
+# Input: string of words with whitespace
+#        only letters a - z utilized
+
+# Output: string in same order but with duplicate words removed
+
+# No extra spaces at the end of returned string
+
 def no_dups(s):
     # Your code here
+    cache = {}
+    words = []
 
+    for word in s.split():
+        if word not in cache:
+            cache[word] = 1
+            words.append(word)
+    return " ".join(words)
 
 
 if __name__ == "__main__":
